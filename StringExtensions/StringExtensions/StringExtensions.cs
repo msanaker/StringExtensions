0b0e02562d6ac5c8c254da21437c2fd7cf0bfe5d
msanaker <matthew@sanaker.com>
2016-05-28 19:28:56 -0400

    Added copyright to file

--- StringExtensions/StringExtensions/StringExtensions.cs
+++ StringExtensions/StringExtensions/StringExtensions.cs
@@ -189,3 +189,21 @@ namespace StringExtensions
         }
     }
 }
+/*
+    Copyright 2016 Matthew Sanaker, matthew@sanaker.com, @msanaker on GitHub
+    
+    This file is part of 'StringExtensions'.
+
+    'StringExtensions' is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    'StringExtensions' is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with userManagement.  If not, see <http://www.gnu.org/licenses/>.
+*/
